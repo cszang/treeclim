8b4798399c8e60034c3603103ca9a5ee24f03e67
znag <rio@fea.st>
2014-02-20 08:46:14 +0100

    easter egg...

--- R/zzz.R
+++ R/zzz.R
@@ -4,6 +4,9 @@
   um <- strsplit(version," ")[[1]]
   version <- um[nchar(um)>0][2]
   hello <- paste("This is bootres2 version ", version, ".", sep = "")
+  afool <- substr(date(), 5, 10) == "Apr 01"
+  afoolt <- ifelse(afool,
+                   "You should not wear this shirt for work...\n", "")
   tips <- c(
     "Need help? See the wiki at https://github.com/znag/bootres2/wiki",
     paste("Coming from bootRes 1.X? See here for a list of changes and for how to adapt your code: https://github.com/znag/bootres2/wiki/Changes-from-bootRes-1.X"),
@@ -11,6 +14,6 @@
   )
   supp <- "(Use suppressPackageStartupMessages to eliminate package startup messages.)"
   tip <- sample(tips, 1)
-  the_tip <- paste(hello, tip, supp, sep = "\n")
+  the_tip <- paste(hello, afoolt, tip, supp, sep = "\n")
   packageStartupMessage(the_tip)
 }
