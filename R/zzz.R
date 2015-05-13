77a85f1bed2fde1e7f7eba1b8a0effa30a7b9c3a
Christian Zang <christian.zang@wzw.tum.de>
2015-05-13 15:11:03 +0200

    simpler startup message

--- R/zzz.R
+++ R/zzz.R
@@ -1,19 +1,13 @@
 .onAttach <- function(...) {
-  library(help=treeclim)$info[[1]] -> version
-  version <- version[pmatch("Version",version)]
-  um <- strsplit(version," ")[[1]]
-  version <- um[nchar(um) > 0][2]
-  hello <- paste("This is treeclim version ", version, ".", sep = "")
-  afool <- substr(date(), 5, 10) == "Apr 01"
-  afoolt <- ifelse(afool,
-                   "You should not wear this shirt for work...\n", "")
+  if (!interactive() || stats::runif(1) > 0.1) return()
+  
   tips <- c(
     "Need help? See the wiki at https://github.com/cszang/treeclim/wiki",
-    paste("Coming from bootRes? See here for a list of changes and for how to adapt your code: https://github.com/cszang/treeclim/wiki/Changes-from-bootRes-1.X"),
-    "See citation('treeclim') for information how to cite this package in your work." 
+    "Coming from bootRes? See here for a list of changes and for how to adapt your code: https://github.com/cszang/treeclim/wiki/Changes-from-bootRes-1.X",
+    "See citation('treeclim') for information how to cite this package in your work."
   )
   supp <- "(Use suppressPackageStartupMessages to eliminate package startup messages.)"
   tip <- sample(tips, 1)
-  the_tip <- paste(hello, afoolt, tip, supp, sep = "\n")
+  the_tip <- paste(tip, supp, sep = "\n")
   packageStartupMessage(the_tip)
 }
