753ff2f5616ca0c0d54dd3da1f62a2013d10dd91
Christian Zang <christian.zang@wzw.tum.de>
2019-03-22 12:04:15 +0100

    recompiled attributes

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -6,6 +6,18 @@
 
 using namespace Rcpp;
 
+// corfun
+SEXP corfun(SEXP x, SEXP y);
+RcppExport SEXP _treeclim_corfun(SEXP xSEXP, SEXP ySEXP) {
+BEGIN_RCPP
+    Rcpp::RObject rcpp_result_gen;
+    Rcpp::RNGScope rcpp_rngScope_gen;
+    Rcpp::traits::input_parameter< SEXP >::type x(xSEXP);
+    Rcpp::traits::input_parameter< SEXP >::type y(ySEXP);
+    rcpp_result_gen = Rcpp::wrap(corfun(x, y));
+    return rcpp_result_gen;
+END_RCPP
+}
 // corfunexact
 SEXP corfunexact(SEXP x, SEXP y, SEXP y0);
 RcppExport SEXP _treeclim_corfunexact(SEXP xSEXP, SEXP ySEXP, SEXP y0SEXP) {
@@ -31,18 +43,6 @@ BEGIN_RCPP
     return rcpp_result_gen;
 END_RCPP
 }
-// corfun
-SEXP corfun(SEXP x, SEXP y);
-RcppExport SEXP _treeclim_corfun(SEXP xSEXP, SEXP ySEXP) {
-BEGIN_RCPP
-    Rcpp::RObject rcpp_result_gen;
-    Rcpp::RNGScope rcpp_rngScope_gen;
-    Rcpp::traits::input_parameter< SEXP >::type x(xSEXP);
-    Rcpp::traits::input_parameter< SEXP >::type y(ySEXP);
-    rcpp_result_gen = Rcpp::wrap(corfun(x, y));
-    return rcpp_result_gen;
-END_RCPP
-}
 // pcor
 SEXP pcor(SEXP x1, SEXP x2, SEXP y, SEXP y0);
 RcppExport SEXP _treeclim_pcor(SEXP x1SEXP, SEXP x2SEXP, SEXP ySEXP, SEXP y0SEXP) {
@@ -54,6 +54,18 @@ BEGIN_RCPP
     Rcpp::traits::input_parameter< SEXP >::type y(ySEXP);
     Rcpp::traits::input_parameter< SEXP >::type y0(y0SEXP);
     rcpp_result_gen = Rcpp::wrap(pcor(x1, x2, y, y0));
+    return rcpp_result_gen;
+END_RCPP
+}
+// respo
+SEXP respo(SEXP x, SEXP y);
+RcppExport SEXP _treeclim_respo(SEXP xSEXP, SEXP ySEXP) {
+BEGIN_RCPP
+    Rcpp::RObject rcpp_result_gen;
+    Rcpp::RNGScope rcpp_rngScope_gen;
+    Rcpp::traits::input_parameter< SEXP >::type x(xSEXP);
+    Rcpp::traits::input_parameter< SEXP >::type y(ySEXP);
+    rcpp_result_gen = Rcpp::wrap(respo(x, y));
     return rcpp_result_gen;
 END_RCPP
 }
@@ -70,26 +82,14 @@ BEGIN_RCPP
     return rcpp_result_gen;
 END_RCPP
 }
-// respo
-SEXP respo(SEXP x, SEXP y);
-RcppExport SEXP _treeclim_respo(SEXP xSEXP, SEXP ySEXP) {
-BEGIN_RCPP
-    Rcpp::RObject rcpp_result_gen;
-    Rcpp::RNGScope rcpp_rngScope_gen;
-    Rcpp::traits::input_parameter< SEXP >::type x(xSEXP);
-    Rcpp::traits::input_parameter< SEXP >::type y(ySEXP);
-    rcpp_result_gen = Rcpp::wrap(respo(x, y));
-    return rcpp_result_gen;
-END_RCPP
-}
 
 static const R_CallMethodDef CallEntries[] = {
+    {"_treeclim_corfun", (DL_FUNC) &_treeclim_corfun, 2},
     {"_treeclim_corfunexact", (DL_FUNC) &_treeclim_corfunexact, 3},
     {"_treeclim_corfunnoboot", (DL_FUNC) &_treeclim_corfunnoboot, 2},
-    {"_treeclim_corfun", (DL_FUNC) &_treeclim_corfun, 2},
     {"_treeclim_pcor", (DL_FUNC) &_treeclim_pcor, 4},
-    {"_treeclim_respoexact", (DL_FUNC) &_treeclim_respoexact, 3},
     {"_treeclim_respo", (DL_FUNC) &_treeclim_respo, 2},
+    {"_treeclim_respoexact", (DL_FUNC) &_treeclim_respoexact, 3},
     {NULL, NULL, 0}
 };
 
